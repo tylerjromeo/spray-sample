aa780aa47321c45845c4e49ddd4f86c074959a08
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2015-05-01 10:49:19 -0400

    adding slick schema creation

--- src/main/scala/com/example/jdbc/DatabaseController.scala
+++ src/main/scala/com/example/jdbc/DatabaseController.scala
@@ -17,6 +17,28 @@ object DatabaseController {
 
   lazy val db = Database.forConfig("postgres")
 
+  lazy val recreate = DBIO.seq(
+    // drop the old tables
+    (users.schema ++ lists.schema).drop,
+    // Create the tables, including primary and foreign keys
+    (users.schema ++ lists.schema).create,
+
+    // Insert some users
+    users ++= Seq(
+      (1, "Tyler"),
+      (2, "Matt"),
+      (3, "Gregg"),
+      (4, "Kevin")
+    ),//TODO figure out auto-incrementing keys
+    // Insert some lists
+    lists ++= Seq(
+      (1, "Tyler's List", 1),
+      (2, "Matt's List", 2),
+      (3, "Gregg's List", 3),
+      (4, "Kevin's List", 4)
+    )
+  )
+
   type User = (Int, String)
 
   class Users(tag: Tag) extends Table[(Int, String)](tag, "users") {
@@ -47,6 +69,12 @@ object DatabaseController {
 
   lazy val lists = TableQuery[Lists]
 
+  def recreateDatabase = {
+    try {
+      db.run(recreate)
+    } finally db.close
+  }
+
   def getAllUserNames = {
     try {
       val query = users.map(_.name)
@@ -61,35 +89,37 @@ object DatabaseController {
     } finally db.close
   }
 
-  def getUsersLists(userId:Int) = {
+  def getUsersLists(userId: Int) = {
     try {
       db.run(lists.filter(_.userId === userId).map(_.name).result)
     } finally db.close
   }
 
   def main(args: Array[String]) {
-//    getAllUserNames onComplete {
-//      case Success(x) => {
-//        println("Users:")
-//        x.foreach(println)
-//      }
-//      case Failure(t) => println("Could not get Users: " + t.getMessage)
-//    }
+    //    getAllUserNames onComplete {
+    //      case Success(x) => {
+    //        println("Users:")
+    //        x.foreach(println)
+    //      }
+    //      case Failure(t) => println("Could not get Users: " + t.getMessage)
+    //    }
 
-//    getAllListNames onComplete {
-//      case Success(x) => {
-//        println("Lists:")
-//        x.foreach(println)
-//      }
-//      case Failure(t) => println("Could not get Lists: " + t.getMessage)
-//    }
+    //    getAllListNames onComplete {
+    //      case Success(x) => {
+    //        println("Lists:")
+    //        x.foreach(println)
+    //      }
+    //      case Failure(t) => println("Could not get Lists: " + t.getMessage)
+    //    }
 
-    getUsersLists(1) onComplete {
-      case Success(x) => {
-        println("Lists for user 1:")
-        x.foreach(println)
-      }
-      case Failure(t) => println("Could not get Lists: " + t.getMessage)
-    }
+    recreateDatabase
+
+    //    getUsersLists(1) onComplete {
+    //      case Success(x) => {
+    //        println("Lists for user 1:")
+    //        x.foreach(println)
+    //      }
+    //      case Failure(t) => println("Could not get Lists: " + t.getMessage)
+    //    }
   }
 }
