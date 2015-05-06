190395a39d925f2ec8eb580aa8517c63bd97e6b2
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2015-05-06 16:22:56 -0400

    adding basic post

--- src/main/scala/com/example/TaskService.scala
+++ src/main/scala/com/example/TaskService.scala
@@ -2,6 +2,7 @@ package com.example
 
 import akka.actor.Actor
 import com.example.jdbc.TaskDAO
+import com.example.jdbc.TaskDAO.Task
 import spray.routing._
 import spray.http._
 import MediaTypes._
@@ -35,6 +36,14 @@ trait TaskService extends HttpService {
           // XML is marshalled to `text/xml` by default, so we simply override here
           onComplete(TaskDAO.getTasks) {
             case Success(value) => complete(value)
+            case Failure(ex) => complete(s"An error occurred: ${ex.getMessage}")
+          }
+        }
+      }
+      post {
+        entity(as[Task]) { task =>
+          onComplete(TaskDAO.addTask(task)) {
+            case Success(value) => complete("")
             case Failure(ex) => complete(s"An error occurred: ${ex.getMessage}")
           }
         }
